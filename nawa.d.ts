8efb0cbbf951aac72c93fa354a65613cbb5fb280
villainhr <villainthr@gmail.com>
2018-07-24 15:14:07 +0800

    feat: fix add .d.ts

---
+++ nawa.d.ts
@@ -0,0 +1,28 @@
+interface NawaOptions {
+    cache?: globalOptions;
+    debug?: boolean;
+    whitelist?:Array<string|RegExp>;
+    precache?:Array<string>;
+    filename?:string;
+    skipWaiting?:boolean;
+}
+
+declare class Network {
+    private routes : object;
+    private swShell;
+
+    public cacheFirst(param : routerOptions) : void;
+    public networkFirst(param : routerOptions) : void;
+    public cacheUpdate(param : routerOptions) : void;
+    public get(param : routerOriginOpt) : void;
+    public post(param : routerOriginOpt) : void;
+    public put(param : routerOriginOpt) : void;
+    public delete(param : routerOriginOpt) : void;
+
+    public use(middleware : asyncMiddleware) : void;
+    public syncUse(middleware : syncMiddleware) : void;
+}
+
+declare class Nawa extends Network {
+    constructor(param : NawaOptions);
+}
