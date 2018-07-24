9daa615c17f330974de5c8b27f9d6150b2bd1bd5
villainhr <villainthr@gmail.com>
2018-07-24 15:40:29 +0800

    feat: fix .d.ts

--- nawa.d.ts
+++ nawa.d.ts
@@ -1,29 +1,33 @@
-interface NawaOptions {
-    cache?: globalOptions;
-    debug?: boolean;
-    whitelist?:Array<string|RegExp>;
-    precache?:Array<string>;
-    filename?:string;
-    skipWaiting?:boolean;
+
+declare var Nawa:nawa.nawaMitt;
+
+declare namespace nawa{
+    interface NawaOptions {
+        cache?: globalOptions;
+        debug?: boolean;
+        whitelist?:Array<string|RegExp>;
+        precache?:Array<string>;
+        filename?:string;
+        skipWaiting?:boolean;
+    }
+    
+    interface Nawa{      
+        cacheFirst(param : routerOptions) : void;
+        networkFirst(param : routerOptions) : void;
+        cacheUpdate(param : routerOptions) : void;
+        get(param : routerOriginOpt) : void;
+        post(param : routerOriginOpt) : void;
+        put(param : routerOriginOpt) : void;
+        delete(param : routerOriginOpt) : void;
+    
+        revoke():void; // when meet error, remove all listeners
+        use(middleware : asyncMiddleware) : void;
+        syncUse(middleware : syncMiddleware) : void;
+    }
+    interface nawaMitt{
+        new(param:NawaOptions):Nawa;
+    }
+    
 }
 
-
-interface NawaContructor{
-    new (param: NawaOptions):Nawa;
-}
-interface Nawa{
-    constructor:NawaContructor;
-    cacheFirst(param : routerOptions) : void;
-    networkFirst(param : routerOptions) : void;
-    cacheUpdate(param : routerOptions) : void;
-    get(param : routerOriginOpt) : void;
-    post(param : routerOriginOpt) : void;
-    put(param : routerOriginOpt) : void;
-    delete(param : routerOriginOpt) : void;
-
-    revoke():void; // when meet error, remove all listeners
-    use(middleware : asyncMiddleware) : void;
-    syncUse(middleware : syncMiddleware) : void;
-}
-
-export default Nawa;
+export default Nawa
